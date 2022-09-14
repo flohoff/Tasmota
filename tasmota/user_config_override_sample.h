c64dea980a5bdde75e4d1097c4c384bc08f49813
Florian Lohoff <f@zz.de>
2022-09-14 10:31:15 +0200

    Add SML to user defined config

--- tasmota/user_config_override_sample.h
+++ tasmota/user_config_override_sample.h
@@ -89,6 +89,15 @@ Examples :
 
 
 
+#ifndef USE_SCRIPT
+#define USE_SCRIPT
+#endif
+#ifndef USE_SML_M
+#define USE_SML_M
+#endif
+#ifdef USE_RULES
+#undef USE_RULES
+#endif
 
 
 #endif  // _USER_CONFIG_OVERRIDE_H_
